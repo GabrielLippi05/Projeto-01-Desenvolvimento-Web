57bff639c59034c9aae30d4cc726bf69fe91edf4
Gabriel Cattuzo <gabrielcattuzo@gmail.com>
2024-11-22 08:02:40 -0300

    firebase update

---
+++ sem uso/Firebase beta/getting-started-with-auth-web/src/index.js
@@ -0,0 +1,104 @@
+import './styles.css';
+import { 
+  hideLoginError, 
+  showLoginState, 
+  showLoginForm, 
+  showApp, 
+  showLoginError, 
+  btnLogin,
+  btnSignup,
+  btnLogout
+} from './ui'
+
+import { initializeApp } from 'firebase/app';
+import { 
+  getAuth,
+  onAuthStateChanged, 
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  connectAuthEmulator
+} from 'firebase/auth';
+
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDsJw8zcPjR1G1f5Y2fmyTVp7zfhrAt31k",
+  authDomain: "projeto-pi-c832e.firebaseapp.com",
+  projectId: "projeto-pi-c832e",
+  storageBucket: "projeto-pi-c832e.firebasestorage.app",
+  messagingSenderId: "219836118232",
+  appId: "1:219836118232:web:4185a2f2488a512fd613ad",
+  measurementId: "G-C8RJKE9Z2B"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Login using email/password
+const loginEmailPassword = async () => {
+  const loginEmail = txtEmail.value
+  const loginPassword = txtPassword.value
+
+  // step 1: try doing this w/o error handling, and then add try/catch
+  await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+
+  // step 2: add error handling
+  // try {
+  //   await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  // }
+  // catch(error) {
+  //   console.log(`There was an error: ${error}`)
+  //   showLoginError(error)
+  // }
+}
+
+// Create new account using email/password
+const createAccount = async () => {
+  const email = txtEmail.value
+  const password = txtPassword.value
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, password)
+  }
+  catch(error) {
+    console.log(`There was an error: ${error}`)
+    showLoginError(error)
+  } 
+}
+
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user)
+      showApp()
+      showLoginState(user)
+
+      hideLoginError()
+      hideLinkError()
+    }
+    else {
+      showLoginForm()
+      lblAuthState.innerHTML = `You're not logged in.`
+    }
+  })
+}
+
+// Log out
+const logout = async () => {
+  await signOut(auth);
+}
+
+btnLogin.addEventListener("click", loginEmailPassword) 
+btnSignup.addEventListener("click", createAccount)
+btnLogout.addEventListener("click", logout)
+
+const auth = getAuth(firebaseApp);
+connectAuthEmulator(auth, "http://localhost:9099");
+
+monitorAuthState();
