4a955542572f462af39b6c702ab4565cdcb95da9
Gabriel Cattuzo <gabrielcattuzo@gmail.com>
2024-12-05 20:47:49 -0300

    UPDATE FIREBASE -> PRÓXIMO UPDATE TRILHOS + API WHATS

--- PROJETO-FINAL/firebase-init.js
+++ PROJETO-FINAL/firebase-init.js
@@ -1,9 +1,3 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyB369p50V_JshNhRQ14yWCCdIpag1xCJEk",
-    authDomain: "uso-de-firebase-app.firebaseapp.com",
-    projectId: "uso-de-firebase-app",
-    storageBucket: "uso-de-firebase-app.firebasestorage.app",
-    messagingSenderId: "234803012921",
-    appId: "1:234803012921:web:0913a1a9988e443e5bf4e8"
   };
 firebase.initializeApp(firebaseConfig);
